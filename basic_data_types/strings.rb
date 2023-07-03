b9ddde7b95d9c27d547cf418179e5b8dede4f960
Kalki V <kalki2706@kali>
2023-07-04 03:50:02 +0530

    add strings substitution method

--- basic_data_types/strings.rb
+++ basic_data_types/strings.rb
@@ -24,3 +24,18 @@ puts ("hello"[0, 2]) #=> "he"
 "hello"[0, 4]   #=> "hell"
 
 "hello"[-1]     #=> "o"
+
+# SUBSTITUTION METHODS
+# These methods perform substitutions:
+#     String#sub: One substitution (or none); returns a new string.
+#     String#sub!: One substitution (or none); returns self.
+#     String#gsub: Zero or more substitutions; returns a new string.
+#     String#gsub!: Zero or more substitutions; returns self.
+
+s = 'hello'
+s.sub(/[aeiou]/, '*')# => "h*llo"
+s.gsub(/[aeiou]/, '*') # => "h*ll*"
+s.gsub(/[aeiou]/, '')# => "hll"
+s.sub(/ell/, 'al')   # => "halo"
+s.gsub(/xyzzy/, '*') # => "hello"
+'THX1138'.gsub(/\d+/, '00') # => "THX00"
