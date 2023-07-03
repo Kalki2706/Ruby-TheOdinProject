6f5372e494a66fe5548b69acff1b6aaa3ec63300
Kalki V <kalki2706@kali>
2023-07-04 03:39:18 +0530

    add string concatination

---
+++ basic_data_types/strings.rb
@@ -0,0 +1,15 @@
+# FUNNY 
+# Strings, strings, wonderful things, use them well and…your app will…grow wings?
+
+# Double and Single Quotation Marks
+# Strings can be formed with either double "" or single'' quotation marks, also known as string literals. 
+
+# CONCATENATION
+# With the plus operator:
+"Welcome " + "to " + "Odin!"    #=> "Welcome to Odin!"
+
+# With the shovel operator:
+"Welcome " << "to " << "Odin!"  #=> "Welcome to Odin!"
+
+# With the concat method:
+"Welcome ".concat("to ").concat("Odin!")  #=> "Welcome to Odin!"
