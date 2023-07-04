4d3b24b9faf05921f8c3a81fc11df3561d1673b5
Kalki V <kalki2706@kali>
2023-07-05 02:27:53 +0530

    add 5th question solution

--- basic_data_types/exercise.rb
+++ basic_data_types/exercise.rb
@@ -27,3 +27,14 @@ puts movie_release_year[:raanjhanna]
 dates = [1975, 2004, 2013, 2001, 1981]
 
 puts dates[-1]
+
+# 5th Question Solution
+# factorial of 5, 6, 7, & 8
+
+puts 5 * 4 * 3 * 2 * 1 #factorial of 5
+
+puts 6 * 5 * 4 * 3 * 2 * 1 #factorail of 6
+
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1 #factorial of 7
+
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 #factorial of 8
