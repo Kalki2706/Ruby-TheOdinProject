1767c574df4c1744d3aefb02e0d8ff4fb00649b9
Kalki V <kalki2706@kali>
2023-07-04 16:36:54 +0530

    state diff between puts and print

---
+++ basic_data_types/symbols.rb
@@ -0,0 +1,5 @@
+# Puts and Print difference
+puts :my_symbol #puts add new line after print
+
+print "Hello world" # print doesn't add new line after print 
+#(Both are same just new line difference)
