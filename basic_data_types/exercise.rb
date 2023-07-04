b08e7413338e36c5327471f7939d76f0adc4f3a8
Kalki V <kalki2706@kali>
2023-07-05 02:12:54 +0530

    add solution for 3rd question

--- basic_data_types/exercise.rb
+++ basic_data_types/exercise.rb
@@ -1,8 +1,8 @@
-# 1st Question
+# 1st Question Solution (most easiest one)
 # STRING CONCATENATION
 puts ("Kalki " + "V")
 
-# 2nd Question
+# 2nd Question Solution (Copied from question website 'cause I didn't understand the question)
 thousands = 4096 / 1000
 hundreds = 4096 % 1000 / 100
 tens = 4096 % 1000 % 1000 /100
@@ -12,3 +12,9 @@ puts thousands
 puts hundreds
 puts tens
 puts ones
+
+# 3rd Question Solution (Solved by me, hihihihihi)
+movie_release_year = {:shole => '1975', :lakshya => '2004', :raanjhanna => '2013', :lagaan => '2001', :naseeb => '1981'} 
+
+puts movie_release_year[:lagaan]
+puts movie_release_year[:raanjhanna]
