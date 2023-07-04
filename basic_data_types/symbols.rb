abd4ccae29c932da88c35daa3a00d0d852296764
Kalki V <kalki2706@kali>
2023-07-04 16:41:48 +0530

    add symbols vs strings

--- basic_data_types/symbols.rb
+++ basic_data_types/symbols.rb
@@ -3,3 +3,13 @@ puts :my_symbol #puts add new line after print
 
 print "Hello world" # print doesn't add new line after print 
 #(Both are same just new line difference)
+
+# SYMBOLS VS STRINGS
+"string" == "string"  #=> true
+
+"string".object_id == "string".object_id  #=> false
+
+:symbol.object_id == :symbol.object_id    #=> true
+
+#<====== REMEMBER: In `Ruby` everything is an object ======>
+
