7fb81965cea1874fdea8bdde06105d9e4eefa1ff
Kalki V <kalki2706@kali>
2023-07-05 01:58:14 +0530

    add solution for 2nd question

---
+++ basic_data_types/exercise.rb
@@ -0,0 +1,14 @@
+# 1st Question
+# STRING CONCATENATION
+puts ("Kalki " + "V")
+
+# 2nd Question
+thousands = 4096 / 1000
+hundreds = 4096 % 1000 / 100
+tens = 4096 % 1000 % 1000 /100
+ones = 4096 % 1000 % 100 % 10
+
+puts thousands
+puts hundreds
+puts tens
+puts ones
