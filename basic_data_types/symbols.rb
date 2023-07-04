d2f86479e1ebc2eaa586c5f6695d416fa7ab2289
Kalki V <kalki2706@kali>
2023-07-04 17:47:03 +0530

    add remove starting comment

--- basic_data_types/symbols.rb
+++ basic_data_types/symbols.rb
@@ -1,9 +1,3 @@
-# Puts and Print difference
-puts :my_symbol #puts add new line after print
-
-print "Hello world" # print doesn't add new line after print 
-#(Both are same just new line difference)
-
 # SYMBOLS VS STRINGS
 "string" == "string"  #=> true
 
