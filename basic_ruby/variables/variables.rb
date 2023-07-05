92b5c5efe97fcffbb68cdf11fe4fd4b00dc75bbb
Kalki V <kalki2706@kali>
2023-07-06 00:21:25 +0530

    add variable operators

--- basic_ruby/variables/variables.rb
+++ basic_ruby/variables/variables.rb
@@ -1,12 +1,23 @@
 # Declaring a variable
 age = 18 #=> 18
 
-age = 18 + 5 #=> 23
+# age = 18 + 5 #=> 23
 
 # age = 33
-puts age
+# puts age
 
 # age = age + 4
+# puts age
 
-age += 4
+# ASSIGNMENT OPERATORS IN VARIABLES
+age += 4 #=> 22
 puts age
+
+age = 18
+puts age -= 2 #=> 16
+
+cash = 10
+puts cash *= 2 #=> 20
+
+temprature = 40
+puts temprature /= 10 #=> 4
