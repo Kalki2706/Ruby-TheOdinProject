11cb120b1e8a10dd64c92caada826b8cd5054a99
Kalki <kalkithewarrior2706@proton.me>
2023-11-28 18:08:26 +0530

    Optimize code

--- 02_Variables/LaunchSchool-Exercises-Solutions/age.rb
+++ 02_Variables/LaunchSchool-Exercises-Solutions/age.rb
@@ -1,6 +1,6 @@
 # Write a program called age.rb that asks a user how old they are and then tells them how old they will be in 10, 20, 30 and 40 years.
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
-puts "In 10 years you will be: #{age.to_i + 10}", "In 20 years you will be: #{age.to_i + 20}", "In 30 years you will be: #{age.to_i + 30}", "In 40 years you will be: #{age.to_i + 40}"
+puts "In 10 years you will be: #{age + 10}", "In 20 years you will be: #{age + 20}", "In 30 years you will be: #{age + 30}", "In 40 years you will be: #{age + 40}"
