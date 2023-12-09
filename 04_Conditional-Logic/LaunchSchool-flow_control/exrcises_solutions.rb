47ea0616b67b8100408687cc7202872615c94b73
Kalki <kalkithewarrior2706@proton.me>
2023-12-10 03:24:43 +0530

    Add solution for question 4 to find correct answers

--- 04_Conditional-Logic/LaunchSchool-flow_control/exrcises_solutions.rb
+++ 04_Conditional-Logic/LaunchSchool-flow_control/exrcises_solutions.rb
@@ -43,4 +43,32 @@ def get_number(digit)
     end
 end
 
-puts get_number(digit = gets.chomp.to_i)
+get_number(digit = gets.chomp.to_i)
+
+# 4. What will each block of code below print to the screen? Write your answer on a piece of paper or in a text editor and then run each block of code to see if you were correct.
+
+# Snippet 1
+'4' == 4 ? puts("TRUE") : puts("FALSE")  # Anaswer: "FALSE"
+
+# Snippet 2
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+# Answer: "Did you get it right?"
+
+# Snippet 3
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright."
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+# Answer: "Alright now!"
