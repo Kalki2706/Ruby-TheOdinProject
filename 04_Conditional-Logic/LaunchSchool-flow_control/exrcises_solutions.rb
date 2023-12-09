bf43ed49ed952d11dc69f6c4f7a788a72cd1f45b
Kalki <kalkithewarrior2706@proton.me>
2023-12-10 03:25:24 +0530

    Add solution of question 5, resolving the error

--- 04_Conditional-Logic/LaunchSchool-flow_control/exrcises_solutions.rb
+++ 04_Conditional-Logic/LaunchSchool-flow_control/exrcises_solutions.rb
@@ -72,3 +72,27 @@ else
   puts "Alrighty!"
 end
 # Answer: "Alright now!"
+
+# 5. When you run the following code... You get the following error message.. "exercise.rb:8: syntax error, unexpected end-of-input, expecting keyword_end", Why do you get this error and how can you fix it?
+
+=begin 
+def equal_to_four(x)
+    if x == 4
+      puts "yup"
+    else
+      puts "nope"
+  end
+  
+  equal_to_four(5)
+=end
+
+# The error message is about ending the function with end keyword 
+def equal_to_four(x)
+    if x == 4
+      puts "yup"
+    else
+      puts "nope"
+  end
+end
+
+equal_to_four(5)
